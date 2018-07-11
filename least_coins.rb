a91a886e5b0ba88f1fba4c8aec2e046042269877
anjalisebastian <anjalisebastian@icloud.com>
2018-07-11 20:39:13 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -9,13 +9,16 @@ def least_coins(cents)
   }
 
 #Code your answer here!
-number_quarters = (cents/25).round
+number_quarters= (cents/25).round
 coins[:quarters]= number_quarters
 left_over_cents= cents- (number_quarters * 25)
-number_dimes = (left_over_cents/10).round
+number_dimes= (left_over_cents/10).round
 coins[:dimes]= number_dimes
-left_over_cents=cents-(number_dimes * 10)
-number_nickels
+left_over_cents= cents-(number_dimes * 10)
+number_nickels= (cents/5).round
+coins[:nickels]= number_nickels
+left_over_cents= cents- (number_nickels * 5)
+
 
 
 
